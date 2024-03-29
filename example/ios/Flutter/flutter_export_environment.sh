438eded91a649cfc37b2ebbdbf67f8220796066b
laevad <djaldave@gmail.com>
2024-03-29 13:37:39 +0800

    Update Flutter and project paths

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\isaac\laevad\dev\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\isaac\StudioProjects\flutter_clean_architecture\example"
+export "FLUTTER_ROOT=C:\Users\Administrator\daveloper\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\WS\flutter_clean_architecture\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
